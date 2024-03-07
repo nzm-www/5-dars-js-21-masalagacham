9c5406c84dc316e49af2e638ecd6cabab7c72d63
nzm-www <nizomiddinabdusattorov50@gmail.com>
2024-03-07 21:53:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+// -----------------------------------------------------------------
+// 17-masala
+// let num1 = +prompt("Yutuq");
+// let num2 = +prompt("Durrang");
+// let num3 = +prompt("Mag`lubiyat");
+// function oyinlarSoni(Yutuq, Durrang, lubiyat) {
+//   let num1 = Yutuq * 3 + Durrang * 1 + lubiyat * 0;
+//   return num1;
+// }
+// console.log(oyinlarSoni(num1, num2, num3));
+// -----------------------------------------------------------------
+//18-masala
+// let matn = prompt("Matin kirting");
+// function desc(matn) {
+//   let natija = "JavaScript" + matn;
+//   return natija;
+// }
+// console.log(desc(matn));
+// -----------------------------------------------------------------
+// 19-masala
+// let num1 = +prompt("asos");
+// let num2 = +prompt("balandlik");
+// function uchburchakуenguzuntomoni(asos, balandlik) {
+//   let natija = asos + balandlik <= 100;
+//   return natija;
+// }
+// console.log(uchburchakуenguzuntomoni(num1, num2));
+// -----------------------------------------------------------------
+// 20-masala
+// function printArray(n) {
+//   let arr = [];
+//   for (let i = 1; i <= n; i++) {
+//     arr.push(i);
+//   }
+//   return arr;
+// }
+// console.log(printArray(prompt("son kirting")));
+// -----------------------------------------------------------------
+// 21-masala
+// let num1 = +prompt("tovq");
+// let num2 = +prompt("qoy");
+// let num3 = +prompt("sigir");
+// function oyoqlarsoni(tovq, qoy, sigir) {
+//   let num1 = tovq * 2 + qoy * 4 + sigir * 4;
+//   return num1;
+// }
+// console.log(oyoqlarsoni(num1, num2, num3));
+// -----------------------------------------------------------------
+// 23-masala
+// let num1 = +prompt("son kirtin");
+// let num2 = +prompt("son kirt");
+// function number(num1, num2) {
+//   let natija = num2 <= num1;
+//   return natija;
+// }
+// console.log(number(num1, num2));
+// -----------------------------------------------------------------
+// FizzBuzz
+
+// let i = +prompt("aksd");
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 15 == 0) console.log("FizzBuzz");
+//   else if (i % 3 == 0) console.log("Fizz");
+//   else if (i % 5 == 0) console.log("Buzz");
+//   else console.log(i);
+// }
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
